f6aa928eb725630febf2d3c07af74007aac9f46c
Daniel Amadeus <daniel@Daniels-MacBook-Pro.local>
2024-09-18 11:46:56 +0800

    deploy

--- database.sql
+++ database.sql
@@ -1,9 +1,5 @@
-CREATE DATABASE user_database;
-
-USE user_database;
-
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
